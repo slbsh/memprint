afb756ffb34067df314fc7a2aa72b0eded96c3a6
slbsh <crafter.antek@gmail.com>
2025-05-02 22:45:08 +0200

    fix: tiny update :)

--- examples/dbg.rs
+++ examples/dbg.rs
@@ -2,35 +2,35 @@ use memprint::MemPrint;
 
 #[derive(MemPrint)]
 struct MyStruct {
-    c: u8,
-    a: i32,
-    b: f32,
+	c: u8,
+	a: i32,
+	b: f32,
 }
 
 impl MyStruct {
-    fn new(a: i32, b: f32, c: u8) -> Self {
-        Self { a, b, c }
-    }
+	fn new(a: i32, b: f32, c: u8) -> Self {
+		Self { a, b, c }
+	}
 }
 
 fn main() {
-    let struc = MyStruct::new(42, 3.14, b'A');
-    
-    // provides a detailed printout of a single struct
-    MyStruct::memprint(&struc);
+	let struc = MyStruct::new(42, 3.14, b'A');
 
-    let mut vec = Vec::new();
+	// provides a detailed printout of a single struct
+	MyStruct::memprint(&struc);
 
-    vec.push(MyStruct::new(42,    3.14, b'A'));
-    vec.push(MyStruct::new(69,    2.71, b'B'));
-    vec.push(MyStruct::new(1337,  1.41, b'C'));
-    vec.push(MyStruct::new(9001,  0.0,  b'D'));
-    vec.push(MyStruct::new(80085, -1.0, b'E'));
+	let mut vec = Vec::new();
 
-    println!();
+	vec.push(MyStruct::new(42,    3.14, b'A'));
+	vec.push(MyStruct::new(69,    2.71, b'B'));
+	vec.push(MyStruct::new(1337,  1.41, b'C'));
+	vec.push(MyStruct::new(9001,  0.0,  b'D'));
+	vec.push(MyStruct::new(80085, -1.0, b'E'));
 
-    // prints a colour coded block of memory, representing the elements of the slice
-    MyStruct::memprint_block(&vec);
+	println!();
 
-    println!();
+	// prints a colour coded block of memory, representing the elements of the slice
+	MyStruct::memprint_block(&vec);
+
+	println!();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,128 +5,124 @@ pub use memprint_derive::MemPrint;
 use colored::{Colorize, Color};
 
 pub struct Field {
-    name: &'static str,
-    span: (usize, usize),
-    type_name: &'static str,
+	name: &'static str,
+	span: (usize, usize),
+	type_name: &'static str,
 }
 
 fn print_slice_bytes(bytes: &[u8], color: Option<Color>) {
-    bytes.iter().for_each(|byte| print!("{}", format!("{:02x}", byte).color(color.unwrap_or(Color::White))));
+	bytes.iter().for_each(|byte| print!("{}", format!("{:02x}", byte)
+		.color(color.unwrap_or(Color::White))));
 }
 
 impl Field {
-    pub fn new<T: ?Sized>(name: &'static str, span: (usize, usize), _: &T) -> Self {
-        Self {
-            name, span,
-            type_name: std::any::type_name::<T>(),
-        }
-    }
+	pub fn new<T: ?Sized>(name: &'static str, span: (usize, usize), _: &T) -> Self {
+		Self {
+			name, span,
+			type_name: std::any::type_name::<T>(),
+		}
+	}
 }
 
 pub trait MemPrint {
-    fn get_fields(&self) -> Vec<Field>;
+	fn get_fields(&self) -> Vec<Field>;
 
-    fn memprint(obj: &Self) where Self: Sized {
-        unsafe {
-            Self::memprint_raw(obj as *const Self);
-        }
-    }
+	fn memprint(obj: &Self) where Self: Sized {
+		unsafe {
+			Self::memprint_raw(obj as *const Self);
+		}
+	}
 
-    unsafe fn memprint_raw(ptr: *const Self) where Self: Sized {
-        use std::cmp;
+	/// # Safety
+	/// ptr must satisfy all the invariants of std::ptr::NonNull
+	unsafe fn memprint_raw(ptr: *const Self) where Self: Sized {
+		use std::cmp;
 
-        let Some(ptr) = std::ptr::NonNull::new(ptr as *mut Self) else {
-            println!("NULLPTR");
-            return;
-        };
+		let ptr = std::ptr::NonNull::new_unchecked(ptr as *mut Self);
 
-        let bytes = std::slice::from_raw_parts(
-            ptr.as_ptr() as *const u8, 
-            std::mem::size_of::<Self>()
-        );
+		let bytes = std::slice::from_raw_parts(
+			ptr.as_ptr() as *const u8, 
+			std::mem::size_of::<Self>()
+		);
 
-        println!("{} {{", std::any::type_name::<Self>());
+		println!("{} {{", std::any::type_name::<Self>());
 
-        let fields = &ptr.as_ref().get_fields();
+		let fields = &ptr.as_ref().get_fields();
 
-        let (mw_name, mw_type) = fields.iter().fold((0, 0), |acc, f| {
-            (cmp::max(acc.0, f.name.len()), cmp::max(acc.1, f.type_name.len()))
-        });
+		let (mw_name, mw_type) = fields.iter().fold((0, 0), |acc, f|
+			(cmp::max(acc.0, f.name.len()), cmp::max(acc.1, f.type_name.len())));
 
-        for field in fields {
-            let pad_name  = mw_name  - field.name.len();
-            let pad_type  = mw_type  - field.type_name.len();
+		fields.iter().for_each(|field| {
+			let pad_name  = mw_name - field.name.len();
+			let pad_type  = mw_type - field.type_name.len();
 
-            print!("    {}: {}", field.name,  " ".repeat(pad_name));
-            print!("{}  {}", field.type_name, " ".repeat(pad_type));
+			print!("    {}: {}", field.name,  " ".repeat(pad_name));
+			print!("{}  {}", field.type_name, " ".repeat(pad_type));
 
-            print_slice_bytes(&bytes[field.span.0..field.span.1], None);
-            println!();
-        }
+			print_slice_bytes(&bytes[field.span.0..field.span.1], None);
+			println!();
+		});
 
-        println!("}}");
-    }
+		println!("}}");
+	}
 
-    fn memprint_simple(obj: &Self) where Self: Sized {
-        unsafe {
-            Self::memprint_simple_raw(obj as *const Self, &obj.get_fields());
-        }
-    }
+	fn memprint_simple(obj: &Self) where Self: Sized {
+		unsafe {
+			Self::memprint_simple_raw(obj as *const Self, &obj.get_fields());
+		}
+	}
 
-    unsafe fn memprint_simple_raw(ptr: *const Self, fields: &[Field]) where Self: Sized {
-        const COLORS: [Color; 8] = [
-            Color::Red,
-            Color::Green,
-            Color::Yellow,
-            Color::Blue,
-            Color::Magenta,
-            Color::Cyan,
-            Color::White,
-            Color::Black,
-        ];
+	unsafe fn memprint_simple_raw(ptr: *const Self, fields: &[Field]) where Self: Sized {
+		const COLORS: [Color; 8] = [
+			Color::Red,
+			Color::Green,
+			Color::Yellow,
+			Color::Blue,
+			Color::Magenta,
+			Color::Cyan,
+			Color::White,
+			Color::Black,
+		];
 
-        for (i, field) in fields.iter().enumerate() {
-            let color = COLORS.get_unchecked(i % COLORS.len());
+		fields.iter().enumerate().for_each(|(i, field)| {
+			let color = COLORS.get_unchecked(i % COLORS.len());
 
-            let bytes = std::slice::from_raw_parts(
-                ptr as *const u8, 
-                std::mem::size_of::<Self>()
-            );
+			let bytes = std::slice::from_raw_parts(
+				ptr as *const u8, 
+				std::mem::size_of::<Self>()
+			);
 
-            print_slice_bytes(&bytes[field.span.0..field.span.1], Some(*color));
-        }
-    }
+			print_slice_bytes(&bytes[field.span.0..field.span.1], Some(*color));
+		})
+	}
 
-    fn memprint_block(arr: &[Self]) where Self: Sized {
-        unsafe {
-            Self::memprint_block_raw(arr.as_ptr(), arr.len());
-        }
-    }
+	fn memprint_block(arr: &[Self]) where Self: Sized {
+		unsafe {
+			Self::memprint_block_raw(arr.as_ptr(), arr.len());
+		}
+	}
 
-    unsafe fn memprint_block_raw(ptr: *const Self, len: usize) where Self: Sized {
-        let Some(ptr) = std::ptr::NonNull::new(ptr as *mut Self) else {
-            println!("NULLPTR");
-            return;
-        };
+	/// # Safety
+	/// ptr must satisfy all the invariants of std::ptr::NonNull
+	unsafe fn memprint_block_raw(ptr: *const Self, len: usize) where Self: Sized {
+		let ptr = std::ptr::NonNull::new_unchecked(ptr as *mut Self);
+		let fields = &ptr.as_ref().get_fields();
 
-        let fields = &ptr.as_ref().get_fields();
+		// heading
+		let mut pads = Vec::with_capacity(fields.len());
 
-        // heading
-        let mut pads = Vec::with_capacity(fields.len());
+		fields.iter().for_each(|f| 
+			pads.push(((f.span.1 - f.span.0) << 1).saturating_sub(f.name.len())));
 
-        fields.iter().for_each(|f| 
-            pads.push(((f.span.1 - f.span.0) << 1).saturating_sub(f.name.len())));
+		fields.iter().enumerate().for_each(|(i, field)|
+			print!("{} {}", field.name, " ".repeat(pads[i].saturating_sub(1))));
 
-        for (i, field) in fields.iter().enumerate() {
-            print!("{} {}", field.name, " ".repeat(pads[i].saturating_sub(1)));
-        }
-        println!();
+		println!();
 
-
-        // block
-        for i in 0..len {
-            Self::memprint_simple_raw(ptr.as_ptr().add(i), &fields);
-            println!();
-        }
-    }
+		// block
+		(0..len).for_each(|i| {
+			Self::memprint_simple_raw(ptr.as_ptr().add(i), &fields);
+			println!();
+		});
+	}
 }
